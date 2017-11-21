40099ce88ee67a8246652bf9f6e51b53f4d5c288
Walter Santiago <wsantiagog1@gmail.com>
2017-11-21 02:43:35 -0500

    Listar y agregar Restaurantes terminados falta modificar

--- app/src/main/java/com/example/santiago/eatfast/Principal.java
+++ app/src/main/java/com/example/santiago/eatfast/Principal.java
@@ -1,15 +1,35 @@
 package com.example.santiago.eatfast;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Principal extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class Principal extends AppCompatActivity implements AdaptadorRestaurante.OnPersonaClickListener{
+
+    private RecyclerView listado;
+    private ArrayList<Restaurante> personas;
+    private Resources res;
+    private AdaptadorRestaurante adapter;
+    private LinearLayoutManager llm;
+    private DatabaseReference databaseReference;
+    private final String BD= "Restaurantes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +38,56 @@ public class Principal extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        listado = (RecyclerView)findViewById(R.id.lstPersonas);
+        res = this.getResources();
+        personas = new ArrayList<>();
+
+        adapter = new AdaptadorRestaurante(this.getApplicationContext(),personas,this);
+        llm = new LinearLayoutManager(this);
+        listado.setLayoutManager(llm);
+        listado.setAdapter(adapter);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child(BD).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                personas.clear();
+                if (dataSnapshot.exists()){
+                    for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                        Restaurante p = snapshot.getValue(Restaurante.class);
+                        personas.add(p);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                Datos.setPersonas(personas);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
+
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+    }
+
+    public void agregar(View v){
+        Intent i = new Intent(Principal.this,CrearRestaurante.class);
+        startActivity(i);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_principal, menu);
-        return true;
-    }
+    public void onPersonaClick(Restaurante p) {
+       /* Intent i = new Intent(Principal.this,ModificarPersona.class);
+        Bundle b = new Bundle();
+        b.putString("id",p.getId());
+        b.putString("cedula",p.getCedula());
+        b.putString("nombre",p.getNombre());
+        b.putString("apellido",p.getApellido());
+        b.putString("foto",p.getFoto());
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        i.putExtra("datos",b);
+        startActivity(i);*/
     }
 }
